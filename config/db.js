de042dfd2e06ecaf4b34397fb06ec2a07581bc3e
Adil101078 <adil101078@gmail.com>
2022-01-12 17:54:22 +0530

    added logger & code refactoring

--- config/db.js
+++ config/db.js
@@ -1,15 +1,38 @@
 import mongoose from 'mongoose'
 import logger from '../middlewares/logger.js'
 
-
-const db = mongoose.connect(process.env.MONGODB_URL, {
+const db = mongoose.createConnection(process.env.MONGODB_URL, {
+    readPreference: 'primaryPreferred',
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then( () => {
-    logger.info('Connected to Database')
-    }, (error) => {
-    logger.error(error)
-   }
-);
+})
+
+db.on('connected', () => {
+	logger.info('Connected to Mongoose Database')
+})
+
+db.on('error', (err) => {
+	logger.debug(`Mongoose connection error for master DB: ${err.message}`)
+})
+
+
+db.on('disconnected', () => {
+	logger.debug('Mongoose connection disconnected for master DB')
+})
+
+
+db.on('reconnected', () => {
+	logger.info('Mongoose connection reconnected for master DB')
+})
+
+process.on('SIGINT', () => {
+	db.close(() => {
+		logger.debug(
+			'Mongoose connection disconnected for master DB through app termination'
+		)
+		process.exit(0)
+	})
+})
+
 
 export default db
